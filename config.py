83faa6c779affff81b31c2214afdd1e6d952f615
see-why <iyadicy@gmail.com>
2022-06-04 19:43:51 +0100

    create db url class

--- config.py
+++ config.py
@@ -11,3 +11,14 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:oyinlola@localhost:5432/fyyerApp'
+
+
+class DatabaseURI:
+
+    # Just change the names of your database and crendtials and all to connect to your local system
+    DATABASE_NAME = "fyyerApp"
+    username = 'postgres'
+    password = 'oyinlola'
+    url = 'localhost:5432'
+    SQLALCHEMY_DATABASE_URI = "postgres://{}:{}@{}/{}".format(
+        username, password, url, DATABASE_NAME)
